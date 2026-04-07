c92c935eaa3cdcde259392d35f57885a3d9aebc9
rordoneza-droid <rordoneza@unemi.edu.ec>
2026-04-06 23:39:05 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
-const CACHE = 'xuri-v1';
-const FILES = ['./index.html'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'xuri-v2';
+const ASSETS = ['./', './xuri_eventuales_v4.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
