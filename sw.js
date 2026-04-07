283671c02a23225769477448a746c9eeb377b7a2
rordoneza-droid <rordoneza@unemi.edu.ec>
2026-04-06 21:31:28 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,26 @@
-const CACHE = 'xuri-v1';
-const FILES = ['./index.html'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'planificador-v1';
+const FILES = ['./planificador.html', './logo.png', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
+      .catch(() => caches.match('./planificador.html'))
+  );
+});
